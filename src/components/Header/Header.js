35f2b1d667b83d30d50750ab7af2933c67d3869c
unknown <islam15-3879@diu.edu.bd>
2022-03-29 22:13:04 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Safe most of your money</h2>
+            <p>by giving miney to me </p>
+        </div>
+    );
+};
+
+export default Header;
