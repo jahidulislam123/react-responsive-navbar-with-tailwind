40804be5a391be0244b6819aab987e08283cca40
unknown <islam15-3879@diu.edu.bd>
2022-03-30 17:28:02 +0600

    finished

--- src/components/PricingOption/PricingOption.js
+++ src/components/PricingOption/PricingOption.js
@@ -7,7 +7,7 @@ const PricingOption = (props) => {
     return (
         <div className='bg-white p-4 rounded-lg'>
             <h2 className='bg-indigo-300 p-2 rounded text-xl font-bold'>{name}</h2>
-
+            <p></p>
             <span className='text-5xl font-bold'>{price}</span>
             <span className='text-xl font-bold text-gray-500'>/mo</span>
             <div >
